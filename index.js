342bb658f705650a993acdad0268893ab9d97717
Ariel Barrios <ariel29111978@hotmail.com>
2022-09-04 18:22:45 -0300

    Agrega las constantes al index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const inputText = document.getElementById('input-text');
+const btnEncrypt = document.getElementById('btn-encrypt');
+const btnDecrypt = document.getElementById('btn-decrypt');
+const containerError = document.getElementById('container-error');
+const containerMessage =document.getElementById('container-message');
 
 console.log('hola');
