a7fec1fe45da153db264f335a48e735843249a15
Ariel Barrios <ariel29111978@hotmail.com>
2022-09-04 19:31:00 -0300

    Agrega la funcionalidad para interactuar con el formulario

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const inputText = document.getElementById('input-text');
+const form = document.getElementById('form')
 const btnEncrypt = document.getElementById('btn-encrypt');
 const btnDecrypt = document.getElementById('btn-decrypt');
 const containerError = document.getElementById('container-error');
 const containerMessage =document.getElementById('container-message');
 
-console.log('hola');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(e.submitter.id, inputText.value);
+  
+  inputText.value = '';
+
+});
