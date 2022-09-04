e68444343594d2db8c0acfc1793014287bb06cf3
Ariel Barrios <ariel29111978@hotmail.com>
2022-09-04 20:49:00 -0300

    Agrega la funcionalidad de mostrar texto o imagen en la sección de mensaje

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ const form = document.getElementById('form')
 const btnEncrypt = document.getElementById('btn-encrypt');
 const btnDecrypt = document.getElementById('btn-decrypt');
 const containerError = document.getElementById('container-error');
-const containerMessage =document.getElementById('container-message');
+const containerMessage = document.getElementById('container-message');
+const messageParagraph = document.getElementById('message__paragraph');
 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  console.log(e.submitter.id, inputText.value);
-  
+  if(inputText.value.trim() !== '') {
+
+    containerError.style.display = 'none';
+    
+    messageParagraph.innerText = inputText.value;
+    
+    containerMessage.style.display = 'flex';
+
+  }else {
+    
+    containerError.style.display = 'block';
+
+    containerMessage.style.display = 'none';
+
+  };
+
   inputText.value = '';
 
 });
