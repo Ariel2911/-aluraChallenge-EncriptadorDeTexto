5cc01d30e8cd9bccb61a507d4a01e75ff937d95f
Ariel Barrios <ariel29111978@hotmail.com>
2022-09-05 10:26:08 -0300

    Agrega la función para encriptar

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
 const inputText = document.getElementById('input-text');
 const form = document.getElementById('form')
-const btnEncrypt = document.getElementById('btn-encrypt');
-const btnDecrypt = document.getElementById('btn-decrypt');
 const containerError = document.getElementById('container-error');
 const containerMessage = document.getElementById('container-message');
 const messageParagraph = document.getElementById('message__paragraph');
 
+const btnEncrypt = document.getElementById('btn-encrypt');
+const btnDecrypt = document.getElementById('btn-decrypt');
+
+//función para encriptar
+const encrypt = (message) => {
+
+  let encryptedMessage = '';
+
+  for(let i=0; i< message.length; i++) {
+
+    let x = '';
+    
+    switch (message.charAt(i)) {
+      case 'a':
+        x = 'ai'
+        break;
+    
+      case 'e':
+        x = 'enter'
+        break;
+    
+      case 'i':
+        x = 'imes'
+        break;
+    
+      case 'o':
+        x = 'ober'
+        break;
+    
+      case 'u':
+        x = 'ufat'
+        break;
+    
+      default:
+        x = message.charAt(i)
+        break;
+    }
+
+    encryptedMessage += x
+
+  };
+
+  return encryptedMessage
+};
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  const message = inputText.value.trim().toLowerCase();
 
-  if(inputText.value.trim() !== '') {
-
-    containerError.style.display = 'none';
+  if(message !== '') {
     
-    messageParagraph.innerText = inputText.value;
+    if(e.submitter.id === 'btn-encrypt') {
+      
+      messageParagraph.innerText = encrypt(message);
+      
+    }
+   
+    containerError.style.display = 'none';
     
     containerMessage.style.display = 'flex';
 
